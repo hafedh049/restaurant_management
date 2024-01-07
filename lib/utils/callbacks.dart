59422196bdc79cb1bb09e79b49a431fab869a862
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-08 00:13:31 +0100

    ModifiedM lib/utils/callbacks.dart AM lib/utils/globals.dart D  lib/utils/methods.dart

--- lib/utils/callbacks.dart
+++ lib/utils/callbacks.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hive/hive.dart';
+import 'package:restaurant_management/utils/globals.dart';
 
 Future<bool> load() async {
   try {
     Hive.init(null);
+    user = await Hive.openBox<dynamic>("user");
+    if (user.get(key)) {}
     await Firebase.initializeApp();
     return true;
   } catch (e) {
