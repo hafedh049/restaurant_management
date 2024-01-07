ce7260ebf33382fde5015a08928601e3cf0d47b8
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-07 22:46:48 +0100

    Modified  lib/main.dart A  lib/views/error.dart A  lib/views/loading.dart M  pubspec.yaml ?? lib/views/resize.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_management/utils/callbacks.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+  ErrorWidget.builder = (FlutterErrorDetails details) => Error(error: details.exception.toString());
   runApp(const Main());
 }
 
