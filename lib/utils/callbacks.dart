fa37d4d91d882267a1c9b27988f3b955123b61d2
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-07 22:36:47 +0100

    ModifiedM lib/main.dart M  lib/utils/callbacks.dart  M pubspec.yaml M  test/widget_test.dart

--- lib/utils/callbacks.dart
+++ lib/utils/callbacks.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hive/hive.dart';
 
 Future<bool> load() async {
   try {
-    Hive.init();
+    Hive.init(null);
+    await Firebase.initializeApp();
     return true;
   } catch (e) {
     return false;
