c148313f9b1e0906da2441232094475fdb52dff0
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-13 17:46:47 +0100

    ModifiedM lib/utils/callbacks.dart M  pubspec.yaml

--- lib/utils/callbacks.dart
+++ lib/utils/callbacks.dart
@@ -17,12 +17,24 @@ Future<bool> load() async {
     //Load Configurations
     configurations = json.decode(await rootBundle.loadString("assets/configurations/configuration.json"));
     //Load Colors
-    Request request = Request("GET", Uri.parse("https://talabatplus.net/api/settings?business_id=${'business_id'}"));
+    Response request = await get(Uri.parse("https://talabatplus.net/api/settings?business_id=${configurations['business_id']}"));
     settings = json.decode(request.body);
     debugPrint(settings.toString());
-    await Firebase.initializeApp();
+    //Initialize Firebase
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyBoQiXy0lpCw8G_3hN88oeJsE3zumuzjtw",
+        authDomain: "restaurant-management-8533a.firebaseapp.com",
+        projectId: "restaurant-management-8533a",
+        storageBucket: "restaurant-management-8533a.appspot.com",
+        messagingSenderId: "807496100298",
+        appId: "1:807496100298:web:a779dda1ff79742a7c0733",
+        measurementId: "G-Z3XHT7EVQ1",
+      ),
+    );
     return true;
   } catch (e) {
+    debugPrint("**");
     debugPrint(e.toString());
     return false;
   }
