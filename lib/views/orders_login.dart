9cdd22fd91bfb98847c4683a74e0c9764982f6aa
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-20 12:20:29 +0100

    ModifiedM lib/views/orders_login.dart

--- lib/views/orders_login.dart
+++ lib/views/orders_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_management/utils/globals.dart';
 
 class OrdersLogin extends StatefulWidget {
   const OrdersLogin({super.key});
@@ -10,6 +11,40 @@ class OrdersLogin extends StatefulWidget {
 class _OrdersLoginState extends State<OrdersLogin> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Container(color: brown, height: MediaQuery.sizeOf(context).height * .3),
+            Expanded(
+              child: Container(
+                color: white,
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text("Login as a Driver", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: white)),
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text("Login as a Manager", style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: white)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: Cro,
+          children: <Widget>[],
+        )
+      ],
+    );
   }
 }
