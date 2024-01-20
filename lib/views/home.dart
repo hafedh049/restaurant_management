593ac6fb3e03b08aa47e353c82a60bead3165ecb
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-20 16:57:12 +0100

    ModifiedM lib/views/home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:restaurant_management/utils/callbacks.dart';
 
 import '../utils/globals.dart';
 
@@ -191,35 +192,43 @@ class _HomeState extends State<Home> {
             ],
           ),
           const SizedBox(height: 30),
-          StatefulBuilder(
-            builder: (BuildContext context, void Function(void Function()) _) {
-              return Wrap(
-                alignment: WrapAlignment.start,
-                crossAxisAlignment: WrapCrossAlignment.start,
-                runAlignment: WrapAlignment.start,
-                runSpacing: 20,
-                spacing: 20,
-                children: <Widget>[
-                  for (int index = 0; index < 20; index += 1)
-                    Container(
-                      height: 300,
-                      width: (MediaQuery.sizeOf(context).width - 48) / 2 - 20,
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), image: const DecorationImage(image: AssetImage("assets/pictures/5.jpg"), fit: BoxFit.cover)),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          const Spacer(),
-                          const Text("Tasty Bites", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: white)),
-                          const SizedBox(height: 10),
-                          Text("✭" * (Random().nextInt(5) + 1), style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: yellow)),
-                        ],
-                      ),
-                    ),
-                ],
-              );
-            },
+          SizedBox(
+            height: 300,
+            child: StatefulBuilder(
+              builder: (BuildContext context, void Function(void Function()) _) {
+                final List<List<int>> pieces = spread(20);
+                return ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, int index) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      for (final int _ in pieces[index]) ...<Widget>[
+                        Container(
+                          height: 300,
+                          width: (MediaQuery.sizeOf(context).width - 48) / 2 - 20,
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), image: const DecorationImage(image: AssetImage("assets/pictures/5.jpg"), fit: BoxFit.cover)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              const Spacer(),
+                              const Text("Tasty Bites", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, color: white)),
+                              const SizedBox(height: 10),
+                              Text("✭" * (Random().nextInt(5) + 1), style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: yellow)),
+                            ],
+                          ),
+                        ),
+                        if (_ != pieces[index].last) const SizedBox(width: 10)
+                      ],
+                    ],
+                  ),
+                  separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 20),
+                  itemCount: pieces.length,
+                );
+              },
+            ),
           ),
         ],
       ),
