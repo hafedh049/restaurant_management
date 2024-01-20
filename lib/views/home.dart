d1c8bdd2e3952afa716fda80fc8ec6545e8efa75
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-20 10:45:27 +0100

    ModifiedM lib/views/home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -12,13 +12,50 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Bootstrap.grid_3x3, size: 15),
-        title: const Text("Restaurant", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[],
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Bootstrap.grid_3x3, size: 15),
+                ),
+                const SizedBox(width: 30),
+                const Text("Restaurant", style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500)),
+              ],
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Bootstrap.pin_map, size: 15),
+                ),
+                const Spacer(),
+                const Text("Add address", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                const Spacer(),
+                Tooltip(
+                  message: "Delivery",
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(FontAwesome.truck_pickup_solid, size: 15),
+                  ),
+                ),
+                Tooltip(
+                  message: "Pickup",
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(FontAwesome.pizza_slice_solid, size: 15),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
