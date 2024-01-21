b700f8e0322ef8d769fc4609717442a566d31494
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-21 11:37:01 +0100

    ModifiedM lib/views/store_products.dart

--- lib/views/store_products.dart
+++ lib/views/store_products.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:restaurant_management/utils/globals.dart';
 
 class StoreProducts extends StatefulWidget {
   const StoreProducts({super.key});
@@ -14,8 +16,25 @@ class _StoreProductsState extends State<StoreProducts> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Row(
-          children: <Widget>[],
+          children: <Widget>[
+            IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Bootstrap.chevron_left, size: 15),
+            ),
+            const Spacer(),
+            const Text("Tasty Bites", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: brown)),
+            const Spacer(),
+            Badge(
+              alignment: Alignment.bottomRight,
+              label: const Text("0", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Bootstrap.shop, size: 15),
+              ),
+            ),
+          ],
         ),
+        const SizedBox(height: 30),
       ],
     );
   }
