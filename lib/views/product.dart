c6ca36f8b3273906772bb27435c8e62956d34eea
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-21 14:11:31 +0100

    ModifiedM lib/views/product.dart

--- lib/views/product.dart
+++ lib/views/product.dart
@@ -20,9 +20,7 @@ class _ProductState extends State<Product> {
           Container(
             height: MediaQuery.sizeOf(context).height * .3,
             width: MediaQuery.sizeOf(context).width,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage(widget.data["picture"]), fit: BoxFit.cover),
-            ),
+            decoration: const BoxDecoration(image: DecorationImage(image: AssetImage("assets/pictures/5.jpg"), fit: BoxFit.cover)),
             padding: const EdgeInsets.all(24),
             alignment: Alignment.topCenter,
             child: Row(
@@ -56,15 +54,47 @@ class _ProductState extends State<Product> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
-                          const Text("0", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+                          const Text("Steak Dinner", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: brown)),
                           const SizedBox(height: 10),
-                          const Text("0", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500)),
+                          const Text("Tasty Bites", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: brown)),
                           const SizedBox(height: 20),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(color: orange, borderRadius: BorderRadius.circular(5)),
+                            child: const Text("Not Deliverable", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: white)),
+                          ),
                         ],
                       ),
                       const Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          const Text("25.99", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: brown)),
+                          const SizedBox(height: 40),
+                          Container(
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(color: brown, borderRadius: BorderRadius.circular(5)),
+                            child: const Text("1 Plate in stock", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: white)),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
+                  const SizedBox(height: 20),
+                  const Divider(thickness: .6, height: .6, color: grey),
+                  const SizedBox(height: 20),
+                  const Text("Juicy steak served with mashed potates and vegetables", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: brown)),
+                  const SizedBox(height: 20),
+                  const Row(
+                    children: <Widget>[
+                      Icon(Bootstrap.chevron_left, size: 25, color: brown),
+                      SizedBox(width: 30),
+                      Text("Reviews", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: brown)),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Text("No reviews yet", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: brown)),
                 ],
               ),
             ),
