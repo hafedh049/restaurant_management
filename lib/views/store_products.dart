0608440c6a45a8fa48b95e335b30f152851de1f5
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-01-21 11:47:02 +0100

    ModifiedM lib/views/store_products.dart

--- lib/views/store_products.dart
+++ lib/views/store_products.dart
@@ -35,6 +35,35 @@ class _StoreProductsState extends State<StoreProducts> {
           ],
         ),
         const SizedBox(height: 30),
+        TextField(
+          readOnly: true,
+          style: const TextStyle(color: brown),
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Bootstrap.search, size: 15, color: brown),
+            contentPadding: const EdgeInsets.all(8),
+            hintStyle: const TextStyle(color: brown, fontSize: 16, fontWeight: FontWeight.w300),
+            hintText: "Search for stores or products",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5),
+              borderSide: const BorderSide(width: 1, color: brown),
+            ),
+          ),
+        ),
+        const SizedBox(height: 30),
+        const Row(
+          children: <Widget>[
+            Icon(Bootstrap.chevron_left, size: 15),
+            SizedBox(width: 30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text("Featured Products", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: brown)),
+                Text("Tasty Bites", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: brown)),
+              ],
+            ),
+          ],
+        ),
       ],
     );
   }
